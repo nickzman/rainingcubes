9d69a1150a2a6bd19dc8df545ad4b066332e4066
Nick Zitzmann <nickzman@gmail.com>
2015-08-29 16:31:57 -0500

    Initial Commit

---
+++ RainingCubes/RainingCubesView.h
@@ -0,0 +1,13 @@
+//
+//  RainingCubesView.h
+//  RainingCubes
+//
+//  Created by Nick Zitzmann on 8/29/15.
+//  Copyright © 2015 Nick Zitzmann. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface RainingCubesView : ScreenSaverView
+
+@end
---
+++ RainingCubes/RainingCubesView.m
@@ -0,0 +1,52 @@
+//
+//  RainingCubesView.m
+//  RainingCubes
+//
+//  Created by Nick Zitzmann on 8/29/15.
+//  Copyright © 2015 Nick Zitzmann. All rights reserved.
+//
+
+#import "RainingCubesView.h"
+
+@implementation RainingCubesView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
